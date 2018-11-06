4039217442390607292e77f98297c48850afc891
Nalipp <nateplipp@gmail.com>
2018-11-06 09:51:51 -0800

    initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h3>App!</h3>
+    );
+  }
+}
+
+export default App;
