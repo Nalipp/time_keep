61c6212f535b071d7cda5b5f1b101cd2929e7fab
Nalipp <nateplipp@gmail.com>
2018-11-12 17:32:18 -0800

    enzyme test setup

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from 'components/app/App';
+import Header from 'components/header/Header';
+import CategoryList from 'components/categories/list';
+import Footer from 'components/footer/Footer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(
+    <App />
+  );
+});
+
+it ('displays the Header component', () => {
+  expect(wrapper.find(Header).length).toEqual(1);
+});
+
+it ('displays the CategoryList component', () => {
+  expect(wrapper.find(CategoryList).length).toEqual(1);
+});
+
+it ('displays the Footer component', () => {
+  expect(wrapper.find(Footer).length).toEqual(1);
 });
